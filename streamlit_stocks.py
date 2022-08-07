8c31fa7e2d5a4a20979d7af5ce8fd5e78e0df181
Eeshan Srivastava <eeshanism@yahoo.com>
2022-08-07 02:06:35 -0700

    add date selectors

--- streamlit_stocks.py
+++ streamlit_stocks.py
@@ -1,10 +1,28 @@
 import yfinance as yf
 import streamlit as st
+import datetime
+from datetime import timedelta
 
 st.write("""
 # Simple Stock Price App
-Type the ticker symbol for which you'd like to see the historical stock closing price
+Type the ticker symbol for which you'd like to see the last 90 Day stock closing price
 """)
+
+# Get Dates
+tDate = datetime.datetime.now()
+l90Date = tDate - timedelta(90)
+
+#tDate = tDate.strftime("%Y-%m-%d")
+#l90Date = l90Date.strftime("%Y-%m-%d")
+
+# Put Date Selector
+d1 = st.sidebar.date_input(
+     "Start Date",
+     l90Date)
+
+d2 = st.sidebar.date_input(
+     "End Date",
+     tDate)
 
 tickerSymbol = st.text_input('Ticker Symbol:','SBUX')
 tickerData = yf.Ticker(tickerSymbol)
@@ -24,7 +42,8 @@ Key Stats:
 * Market Cap: {}
 """.format( tickerLongName, tickerEmployees, tickerRevenue, tickerMarketCap))
 
-tickerDf = tickerData.history(period='1d', start='2019-01-01', end='2022-8-05')
+# Fetch Date
+tickerDf = tickerData.history(period='1d', start=d1, end=d2)
 
 st.write("""
 ### Stock Closing Price
