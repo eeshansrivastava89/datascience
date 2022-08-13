2bd78a3b16d96610e372bdd859b1ab44dcea0ffa
Eeshan Srivastava <eeshanism@yahoo.com>
2022-08-13 10:48:39 -0700

    updates

--- sql/case1_danny_diner_SQL.sql
+++ sql/case1_danny_diner_SQL.sql
@@ -2,6 +2,8 @@
    Case Study Questions
    --------------------*/
 
+-- https://8weeksqlchallenge.com/case-study-1/
+
 -- 1. What is the total amount each customer spent at the restaurant?
 -- 2. How many days has each customer visited the restaurant?
 -- 3. What was the first item from the menu purchased by each customer?
