d352c4c6b139d3ce3affefa32325e69ea8e8a330
Eeshan Srivastava <eeshanism@yahoo.com>
2022-08-13 22:58:29 -0700

    adding case 2 files

---
+++ sql/case2_pizza_SQL.sql
@@ -0,0 +1,46 @@
+/*
+
+Source: https://8weeksqlchallenge.com/case-study-2/ 
+
+Section 1: 
+
+A. Pizza Metrics
+How many pizzas were ordered?
+How many unique customer orders were made?
+How many successful orders were delivered by each runner?
+How many of each type of pizza was delivered?
+How many Vegetarian and Meatlovers were ordered by each customer?
+What was the maximum number of pizzas delivered in a single order?
+For each customer, how many delivered pizzas had at least 1 change and how many had no changes?
+How many pizzas were delivered that had both exclusions and extras?
+What was the total volume of pizzas ordered for each hour of the day?
+What was the volume of orders for each day of the week?
+
+*/
+
+-- Samples
+
+SELECT * FROM pizza_runner.customer_orders
+SELECT * FROM pizza_runner.runner_orders
+SELECT * FROM pizza_runner.pizza_names
+SELECT * FROM pizza_runner.pizza_toppings
+SELECT * FROM pizza_runner.pizza_recipes
+SELECT * FROM pizza_runner.runners
+
+-- Section 1: Pizza Metrics
+
+-- Q1: How many pizzas were ordered?
+
+
+
+
+
+-- Q2: How many unique customer orders were made?
+-- Q3: How many successful orders were delivered by each runner?
+-- Q4: How many of each type of pizza was delivered?
+-- Q5: How many Vegetarian and Meatlovers were ordered by each customer?
+-- Q6: What was the maximum number of pizzas delivered in a single order?
+-- Q7: For each customer, how many delivered pizzas had at least 1 change and how many had no changes?
+-- Q8: How many pizzas were delivered that had both exclusions and extras?
+-- Q9: What was the total volume of pizzas ordered for each hour of the day?
+-- Q10: What was the volume of orders for each day of the week?
